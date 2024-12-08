30bc815901e41f53aa06fa31fd648cf975b81f3f
Conan Truong <truong.conan@gmail.com>
2024-12-08 10:49:47 -0800

    Change sx to stx

--- source/GcLib/gstd/GstdConstant.hpp
+++ source/GcLib/gstd/GstdConstant.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "../pch.h"
 
-const std::wstring DNH_EXE_NAME = L"th_dnh_ph3sx.exe";
+const std::wstring DNH_EXE_NAME = L"th_dnh_ph3stx.exe";
 const std::wstring DNH_VERSION = L"v1.33a-pre2";
 
 constexpr const uint64_t _GAME_VERSION_RESERVED = /*e*/621;		//OWO!!!!!
